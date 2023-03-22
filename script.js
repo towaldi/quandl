02aab50819a6c765cba575fb5b2ccff4a43bb4a1
towaldi <tobiaswalddoerfer@gmail.com>
2023-03-22 10:28:18 +0100

    load data (text) from server + convert in JSON

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Variable is constant -> name in capitals + underscore
 const API_KEY = 'hFUgzoqR8cRWMz6Y3ERX';
+
+async function loadShareprices() {
+    let url = 'https://data.nasdaq.com/api/v3/datasets/BITFINEX/LUNAF0USTF0?start_date=2022-05-22&end_date=2022-05-22&api_key=hFUgzoqR8cRWMz6Y3ERX';
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    console.log('API answers;', responseAsJson['dataset']['data'][0]);
+}
